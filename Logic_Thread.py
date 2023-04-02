95b7760cc01284d20469dff27777a94a16c84be8
hashen47 <89251246+hashen47@users.noreply.github.com>
2023-04-02 20:56:43 +0530

    create this class to run copy or move functionallities (Logic class functionilities pararally with the main thread which run the tkinter(gui) main loop

---
+++ Logic_Thread.py
@@ -0,0 +1,18 @@
+from threading import Thread
+from Logic import Categorizer
+
+
+
+class Logic_Thread(Thread):
+    def __init__(self, src, dst, update_func):
+        super().__init__()
+        self.src = src
+        self.dst = dst
+        self.update_func = update_func
+        self.daemon = True
+
+
+    def run(self):
+        self.categorizer = Categorizer(self.src, self.dst, self.update_func)
+
+
